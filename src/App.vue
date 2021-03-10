b532da4b08a205bc8ff1c429c78651d7b46b6bec
babyazalea <usun12@hotmail.com>
2021-03-10 14:58:06 +0900

    using getters

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <base-container title="Vuex">
     <the-counter></the-counter>
+    <favorite-value></favorite-value>
     <button @click="ChangeCounterdOne">Add 10</button>
     <change-counter></change-counter>
   </base-container>
@@ -9,13 +10,15 @@
 <script>
 import BaseContainer from './components/BaseContainer.vue';
 import ChangeCounter from './components/ChangeCounter.vue';
+import FavoriteValue from './components/FavoriteValue.vue';
 import TheCounter from './components/TheCounter.vue';
 
 export default {
   components: {
     BaseContainer,
     TheCounter,
-    ChangeCounter
+    ChangeCounter,
+    FavoriteValue
   },
   methods: {
     ChangeCounterdOne() {
--- src/main.js
+++ src/main.js
@@ -16,6 +16,22 @@ const store = createStore({
     increase(state, payload) {
       state.counter = state.counter + payload.value;
     }
+  },
+  getters: {
+    finalCounter(state) {
+      return state.counter * 3;
+    },
+    nomalizedCounter(_, getters) {
+      const finalCounter = getters.finalCounter;
+      if (finalCounter < 0) {
+        return 0;
+      }
+
+      if (finalCounter > 100) {
+        return 100;
+      }
+      return finalCounter;
+    }
   }
 });
 
